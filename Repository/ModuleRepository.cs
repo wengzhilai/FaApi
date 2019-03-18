083d585668610f1d93c87c2e3ce5daa152f8b4d9
wendellE450 <wendellE450@example.com>
2019-03-19 00:41:21 +0800

    添加模块

--- Repository/ModuleRepository.cs
+++ Repository/ModuleRepository.cs
@@ -42,15 +42,16 @@ namespace Repository
 
         private List<KTV> GetChildItems(IEnumerable<FaModuleEntity> inList, int? parentId)
         {
-            var childList = inList.Where(i => i.PARENT_ID == parentId);
+            var childList = inList.Where(i => i.PARENT_ID == parentId).ToList();
             List<KTV> reObj = new List<KTV>();
             foreach (var item in childList)
             {
                 var addItem = new KTV();
                 addItem.K = item.ID.ToString();
                 addItem.V = item.NAME;
-                addItem.T=item.LOCATION;
+                addItem.T = item.LOCATION;
                 addItem.child = GetChildItems(inList, item.ID);
+                reObj.Add(addItem);
             }
             return reObj;
         }
@@ -83,18 +84,26 @@ namespace Repository
         public async Task<Result<KTV>> GetMenuByRoleId(List<int> roleIdList)
         {
             Result<KTV> reObj = new Result<KTV>();
-            DapperHelper<FaRoleModuleEntityView> roleModule=new DapperHelper<FaRoleModuleEntityView>();
-            var allModel = await roleModule.FindAll(string.Format( "a.ROLE_ID in ({0})",string.Join(",",roleIdList)));
-            reObj.DataList = GetChildItems(Fun.ClassListToCopy<FaRoleModuleEntityView,FaModuleEntity>(allModel.ToList()), null);
+            if (!roleIdList.Contains(1))
+            {
+                DapperHelper<FaRoleModuleEntityView> roleModule = new DapperHelper<FaRoleModuleEntityView>();
+                var allModel = await roleModule.FindAll("");
+                allModel = await roleModule.FindAll(string.Format("a.ROLE_ID in ({0})", string.Join(",", roleIdList)));
+                reObj.DataList = GetChildItems(Fun.ClassListToCopy<FaRoleModuleEntityView, FaModuleEntity>(allModel.ToList()), null);
+            }
+            else
+            {
+                reObj.DataList = GetChildItems(await new DapperHelper<FaModuleEntity>().FindAll(""), null);
+            }
             return reObj;
         }
 
         public async Task<Result<KTV>> GetMGetMenuByUserId(int userId)
         {
-            
-            DapperHelper<FaUserRoleEntityView> userRole=new DapperHelper<FaUserRoleEntityView>();
-            var allRole = await userRole.FindAll(i=>i.USER_ID==userId);
-            return await GetMenuByRoleId(allRole.Select(i=>i.ROLE_ID).ToList());
+
+            DapperHelper<FaUserRoleEntityView> userRole = new DapperHelper<FaUserRoleEntityView>();
+            var allRole = await userRole.FindAll(i => i.USER_ID == userId);
+            return await GetMenuByRoleId(allRole.Select(i => i.ROLE_ID).ToList());
         }
     }
 }
