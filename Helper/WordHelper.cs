f4e3d5823923888f3e405bf110c11a900ec37301
wendellE450 <wendellE450@example.com>
2019-07-12 00:26:10 +0800

    tt

--- Helper/WordHelper.cs
+++ Helper/WordHelper.cs
@@ -9,9 +9,6 @@ using NPOI.XWPF.UserModel;
 
 public class WordHelper
 {
-    private XWPFDocument doc = null;
-    private string docFile = null;
-
     private static int SHEET_NUM = 0;
     private static int ROW_NUM = 0;
     private static int CELL_NUM = 0;
@@ -21,56 +18,23 @@ public class WordHelper
 
     public WordHelper(string path)
     {
-        UpdateEmbeddedDoc(path);
-        UpdateEmbeddedDoc1();
-        CheckUpdatedDoc();
+        var doc = MakeXWPFDocument(path);
+        UpdateEmbeddedDoc1(doc);
+        SaveDoc(doc, path);
+        // CheckUpdatedDoc();
     }
 
-    public void CheckUpdatedDoc()
+    public XWPFDocument MakeXWPFDocument(String filename)
     {
-        IWorkbook workbook = null;
-        ISheet sheet = null;
-        IRow row = null;
-        NPOI.SS.UserModel.ICell cell = null;
-        PackagePart pPart = null;
-        IEnumerator<PackagePart> pIter = null;
-        List<PackagePart> embeddedDocs = this.doc.GetAllEmbedds();
-        if (embeddedDocs != null && embeddedDocs.Count != 0)
-        {
-            pIter = embeddedDocs.GetEnumerator();
-            while (pIter.MoveNext())
-            {
-                pPart = pIter.Current;
-                if (pPart.PartName.Extension.Equals(BINARY_EXTENSION) ||
-                        pPart.PartName.Extension.Equals(OPENXML_EXTENSION))
-                {
-                    workbook = WorkbookFactory.Create(pPart.GetInputStream());
-                    sheet = workbook.GetSheetAt(SHEET_NUM);
-                    row = sheet.GetRow(ROW_NUM);
-                    cell = row.GetCell(CELL_NUM);
-                    // Assert.AreEqual(cell.NumericCellValue, NEW_VALUE);
-                }
-            }
-        }
-    }
-
-    public void UpdateEmbeddedDoc(String filename)
-    {
-        this.docFile = filename;
         FileStream fis = null;
         if (!File.Exists(filename))
         {
-            throw new FileNotFoundException("The Word dcoument " +
-                    filename +
-                    " does not exist.");
+            throw new FileNotFoundException("文档 " + filename + " 不存在.");
         }
         try
         {
-
-            // Open the Word document file and instantiate the XWPFDocument
-            // class.
-            fis = new FileStream(this.docFile, FileMode.Open);
-            this.doc = new XWPFDocument(fis);
+            fis = new FileStream(filename, FileMode.Open);
+            return new XWPFDocument(fis);
         }
         finally
         {
@@ -91,68 +55,24 @@ public class WordHelper
         }
     }
 
-    public void UpdateEmbeddedDoc1()
+    public XWPFDocument UpdateEmbeddedDoc1(XWPFDocument doc)
     {
+        var cell = doc.Tables[1].Rows[0].GetCell(0).Tables[0].Rows[1].GetCell(0);
+        cell.RemoveParagraph(0);
+        this.AddElder(cell, "第七世");
+        this.AddName(cell, "翁志来");
+        this.AddRemark(cell, "出生于1981年3月13");
+        return doc;
+    }
 
-        IList<XWPFTable> embeddedDocs = this.doc.Tables;
-        if (embeddedDocs != null && embeddedDocs.Count != 0)
-        {
-            // foreach (var para in doc.Paragraphs)
-            // {
-            //     ReplaceKey(para);
-            // }
-            //遍历表格
-            var tables = doc.Tables;
-            // foreach (var table in tables)
-            // {
-            //     foreach (var row in table.Rows)
-            //     {
-            //         foreach (var cell in row.GetTableCells())
-            //         {
-            //             foreach (var para in cell.Paragraphs)
-            //             {
-            //                 ReplaceKey(para);
-            //             }
-            //         }
-            //     }
-            // }
-            //表格内容
-            var tablesChild = doc.Tables[0].Rows[0].GetCell(1).Tables;
-            //第二排所有文字
-            var paragraphs = tablesChild[0].Rows[1].GetCell(0).Paragraphs;
-            var txtList = paragraphs.Select(x => x.ParagraphText).ToList();
-
-            var cell = tablesChild[0].Rows[2].GetCell(0);
-            XWPFParagraph p3 = cell.AddParagraph();
-            p3.IndentFromLeft=113;
-            p3.IndentFromRight=113;
-            p3.Alignment=ParagraphAlignment.CENTER;
-            var r= p3.CreateRun();
-            r.SetText("翁志来");
-            r.FontFamily="黑体";
-            r.FontSize=18;
-
-            XWPFParagraph p4 = cell.AddParagraph();
-            p4.IndentFromLeft=113;
-            p4.IndentFromRight=113;
-            var r4= p4.CreateRun();
-            r4.SetText("生殁未详生殁未详生殁未详生殁未详");
-            r4.FontFamily="楷体";
-            r4.FontSize=14;
-
-        //    tablesChild[0].Rows[2].GetCell(0).Paragraphs[0].Runs[0].SetText("第二十七");
-
-            // tablesChild[0].Rows[2].GetCell(0).SetText("asdfadf");
-
-            // ReplaceKeyTable(doc.Tables);
-
-        }
-
-        // Finally, write the newly modified Word document out to file.
-        string file = Path.GetFileNameWithoutExtension(this.docFile) + "tmp" + Path.GetExtension(this.docFile);
-        this.doc.Write(new FileStream(file, FileMode.CreateNew));
-        this.doc.Close();
-        this.doc=null;
+    public void SaveDoc(XWPFDocument doc, string path)
+    {
+        string file = Path.GetFileNameWithoutExtension(path) + "tmp" + Path.GetExtension(path);
+        var fs = new FileStream(file, FileMode.Create);
+        doc.Write(fs);
+        fs.Close();
+        fs.Dispose();
+        doc.Close();
     }
 
     private void ReplaceKeyTable(IList<XWPFTable> tables)
@@ -187,5 +107,60 @@ public class WordHelper
             runs[i].SetText(text + 2, 0);
         }
     }
+
+    /// <summary>
+    /// 添加辈份
+    /// </summary>
+    /// <param name="cell"></param>
+    /// <param name="txt"></param>
+    private void AddElder(XWPFTableCell cell, string txt)
+    {
+        XWPFParagraph p3 = cell.AddParagraph();
+        p3.IndentFromLeft = 113;
+        p3.IndentFromRight = 113;
+        p3.Alignment = ParagraphAlignment.CENTER;
+        var r = p3.CreateRun();
+        r.GetCTR().AddNewRPr().bdr = new NPOI.OpenXmlFormats.Wordprocessing.CT_Border()
+        {
+            val = NPOI.OpenXmlFormats.Wordprocessing.ST_Border.thinThickSmallGap,
+            sz = 24
+        };
+        r.SetText(txt);
+        r.FontFamily = "黑体";
+        r.FontSize = 18;
+    }
+
+    /// <summary>
+    /// 添加名称
+    /// </summary>
+    /// <param name="cell"></param>
+    /// <param name="txt"></param>
+    private void AddName(XWPFTableCell cell, string txt)
+    {
+        XWPFParagraph p3 = cell.AddParagraph();
+        p3.IndentFromLeft = 113;
+        p3.IndentFromRight = 113;
+        p3.Alignment = ParagraphAlignment.CENTER;
+        var r = p3.CreateRun();
+        r.SetText(txt);
+        r.FontFamily = "黑体";
+        r.FontSize = 18;
+    }
+    /// <summary>
+    /// 添加说明
+    /// </summary>
+    /// <param name="cell"></param>
+    /// <param name="txt"></param>
+    private void AddRemark(XWPFTableCell cell, string txt)
+    {
+        XWPFParagraph p4 = cell.AddParagraph();
+        p4.IndentFromLeft = 113;
+        p4.IndentFromRight = 113;
+        var r4 = p4.CreateRun();
+        r4.SetText(txt);
+        r4.FontFamily = "楷体";
+        r4.FontSize = 14;
+    }
+
 
 }
